32d3eefb23cab35dfe3a1550c6f502a8441b548e
drgimatt <boxrox12@gmail.com>
2023-10-24 19:44:19 +0800

    Updated Create, Update, delete routines

--- DogAdopt-BackEnd/demo/src/main/java/com/rijai/LocationApi/controller/DogController.java
+++ DogAdopt-BackEnd/demo/src/main/java/com/rijai/LocationApi/controller/DogController.java
@@ -1,15 +1,16 @@
 package com.rijai.LocationApi.controller;
 
 import com.rijai.LocationApi.model.Dog;
-import com.rijai.LocationApi.model.Account;
-import com.rijai.LocationApi.service.IAccountService;
 import com.rijai.LocationApi.service.IDogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -18,7 +19,7 @@ import java.util.List;
 public class DogController {
     @Autowired
     private IDogService dogService;
-    private IAccountService accountService;
+
 
     @RequestMapping("/api/dogs")
     public List<Dog> findDogs(){
@@ -31,12 +32,14 @@ public class DogController {
     }
 
     @RequestMapping(value="/api/add-dog", method= RequestMethod.POST)
-    public Dog addDogSubmit(@RequestBody Dog dog) {
+    public Dog addDogSubmit(@RequestParam("photo") MultipartFile file, @RequestParam("name") String name, @RequestParam("breed") String breed, @RequestParam("age") int age, @RequestParam("doa") Date doa, @RequestParam("personality") String personality, @RequestParam("status") String status) throws IOException {
+        Dog dog = new Dog(null, file.getBytes(), name, breed, age, doa, personality, status);
         return dogService.addDog(dog);
     }
 
     @RequestMapping(value="/api/update-dog/{id}", method=RequestMethod.PUT)
-    public Dog updateDog(@PathVariable Long id, @RequestBody Dog dog) {
+    public Dog updateDog(@RequestParam("id") Long id,@RequestParam("photo") MultipartFile file, @RequestParam("name") String name, @RequestParam("breed") String breed, @RequestParam("age") int age, @RequestParam("doa") Date doa, @RequestParam("personality") String personality, @RequestParam("status") String status) throws IOException {
+        Dog dog = new Dog(id, file.getBytes(), name, breed, age, doa, personality, status);
         return dogService.updateDog(id, dog);
     }
     @RequestMapping(value = "/api/delete-dog/{id}", method = {RequestMethod.DELETE, RequestMethod.POST})
--- DogAdopt-BackEnd/demo/src/main/java/com/rijai/LocationApi/service/DogService.java
+++ DogAdopt-BackEnd/demo/src/main/java/com/rijai/LocationApi/service/DogService.java
@@ -1,6 +1,5 @@
 package com.rijai.LocationApi.service;
 
-import com.rijai.LocationApi.model.Account;
 import com.rijai.LocationApi.model.Dog;
 import com.rijai.LocationApi.repository.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
